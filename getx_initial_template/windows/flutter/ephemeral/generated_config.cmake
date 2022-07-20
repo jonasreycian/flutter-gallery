cbf58cb2f0afeb51ad20423c9d321f6b4c55ded2
Jonas Reycian B. Saraosos <jonasreycian@gmail.com>
2022-07-20 15:53:52 +0800

    Adding files

---
+++ getx_initial_template/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\git\\flutter-gallery\\getx_initial_template" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\git\\flutter-gallery\\getx_initial_template"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\git\\flutter-gallery\\getx_initial_template\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\git\\flutter-gallery\\getx_initial_template"
+  "FLUTTER_TARGET=D:\\git\\flutter-gallery\\getx_initial_template\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\git\\flutter-gallery\\getx_initial_template\\.dart_tool\\package_config.json"
+)
